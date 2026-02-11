c3739bdb6db93b7673595cf4d9d8ed1c9183f26c
zsolt <beki.zsolt@petrik.hu>
2026-02-11 17:01:03 +0100

    Pakli hozzzáadva

---
+++ Pakli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunBarlang
+{
+    internal class Pakli
+    {
+        List<Kartya> list;
+
+        public Pakli(List<Kartya> list)
+        {
+            this.List = list;
+        }
+
+        internal List<Kartya> List { get => list; set => list = value; }
+    }
+}
