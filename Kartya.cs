bf2c1c2b2b96b58ab3d1787b0adc813563eb8cba
zsolt <beki.zsolt@petrik.hu>
2026-02-11 16:58:52 +0100

    Kartya.cs hozáadva

---
+++ Kartya.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunBarlang
+{
+    internal class Kartya
+    {
+
+        private int ertek;
+        private int jel;
+
+        public Kartya(int ertek, int jel)
+        {
+            this.Ertek = ertek;
+            this.Jel = jel;
+        }
+
+        public int Ertek { get => ertek; set => ertek = value; }
+        public int Jel { get => jel; set => jel = value; }
+    }
+}
