7d80259960f64736bf5b0c5db543c246c008f147
zsolt <beki.zsolt@petrik.hu>
2026-02-11 17:58:14 +0100

    kártyaformázás hozzáadva

--- Kartya.cs
+++ Kartya.cs
@@ -22,6 +22,64 @@ namespace BunBarlang
 
         public int Jel { get => jel; set => jel = value; }
         public int Ertek { get => ertek; set => ertek = value; }
-        
+
+
+        public override string ToString()
+        {
+            switch (jel) 
+            {
+                case 1: return $@"
+┌─────────────┐
+│{ertek}            │
+│      /\     │
+│     /  \    │
+│    /    \   │
+│    \    /   │
+│     \__/    │
+│      ||     │
+│      ||    {ertek}│
+└─────────────┘              
+                                    ";break;
+                case 2: return $@"
+┌─────────────┐
+│{ertek}            │
+│    /\  /\   │
+│   /  \/  \  │
+│    \      / │
+│     \    /  │
+│      \  /   │
+│       \/    │
+│            {ertek}│
+└─────────────┘         
+                                    "; break;
+                case 3: return $@"
+┌─────────────┐
+|{ertek}            |
+│     / \     │
+│    /   \    │
+│   /     \   │
+│   \     /   │
+│    \   /    │
+│     \ /     │
+│            {ertek}│
+└─────────────┘          
+                                    "; break;
+                case 4: return $@"
+┌─────────────┐
+│{ertek}     __     │
+│     /  \    │
+│   _|____|_  │
+│  /   /\   \ │
+│  \___\/___/ │
+│      ||     │
+│      ||     │
+│             │
+│            {ertek}│
+└─────────────┘             
+                                    "; break;
+                default: return "hiba"; break;
+            }
+
+        }
     }
 }
